a7f702cdf7e50a9081122bb690d0900192b55222
Beslan06 <bor.mm2011@yandex.ru>
2023-08-10 13:53:23 +0300

    Immersion_in_Python

--- 1.py
+++ 1.py
@@ -1,11 +1,12 @@
-def is_prime(number):
-    return number > 1 and all(number % i != 0 for i in range(2, int(number**0.5) + 1))
+a, b, c = sorted(map(float, input("Введите длины сторон через пробел: ").split()))
 
-try:
-    number = int(input("Введите число (от 2 до 100000): "))
-    if 2 <= number <= 100000:
-        print(f"{number} - {'простое' if is_prime(number) else 'составное'} число.")
+if a + b > c:
+    print("Треугольник существует.")
+    if a == b == c:
+        print("Треугольник равносторонний.")
+    elif a == b or a == c or b == c:
+        print("Треугольник равнобедренный.")
     else:
-        print("Число должно быть от 2 до 100000.")
-except ValueError:
-    print("Ошибка ввода. Введите целое число.")
+        print("Треугольник разносторонний.")
+else:
+    print("Треугольник с такими сторонами не существует.")
